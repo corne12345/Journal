7fefe06c7e32473364a69ea3e55681b009caf8de
Corne <39339251+corne12345@users.noreply.github.com>
2018-12-03 14:38:18 +0100

    Everything works, just need to review it all

---
+++ app/src/main/java/com/example/corne/journal/InputActivity.java
@@ -0,0 +1,48 @@
+package com.example.corne.journal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import java.util.Calendar;
+
+public class InputActivity extends AppCompatActivity {
+
+    private static final String TAG = "Input Activity";
+    private Button mConfirmButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+
+        mConfirmButton = (Button) findViewById(R.id.confirmationButton);
+        mConfirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView title = findViewById(R.id.editTitle);
+                String titleString = title.getText().toString();
+
+                TextView content = findViewById(R.id.editText);
+                String contentString = content.getText().toString();
+
+                RadioGroup mood = findViewById(R.id.radioGroup);
+                int moodInt = mood.getCheckedRadioButtonId();
+                RadioButton checkedMood = (RadioButton) findViewById(moodInt);
+                String moodString = (String) checkedMood.getText().toString();
+
+                JournalEntry entry = new JournalEntry(titleString, contentString, moodString);
+                EntryDatabase db = EntryDatabase.getInstance(getApplicationContext());
+                db.insert(entry);
+
+                Intent intent = new Intent(InputActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
